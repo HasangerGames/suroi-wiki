735dc636efa31285ebbff0cfcbdfb3a70d1b6eae
Compositr <compositr@compositr.dev>
2023-11-05 23:04:42 +1100

    lint: linter

--- app/(wiki)/weapons/layout.tsx
+++ app/(wiki)/weapons/layout.tsx
@@ -1,6 +1,5 @@
-export default function WeaponsLayout({children}: React.PropsWithChildren) {
-    return <main className="grid grid-cols-6 lg:grid-cols-8 gap-4">
-      {children}
-    </main>
-    
+export default function WeaponsLayout({ children }: React.PropsWithChildren) {
+  return (
+    <main className="grid grid-cols-6 lg:grid-cols-8 gap-4">{children}</main>
+  );
 }
