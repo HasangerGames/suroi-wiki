44110f211952d05d1b97d94ea06cec128aaf34c6
Compositr <compositr@compositr.dev>
2023-11-06 07:51:08 +1100

    refactor: add (articles) segement for layout purposes

--- app/(wiki)/(articles)/layout.tsx
+++ app/(wiki)/(articles)/layout.tsx
@@ -1,4 +1,4 @@
-export default function WeaponsLayout({ children }: React.PropsWithChildren) {
+export default function ArticleLayout({ children }: React.PropsWithChildren) {
   return (
     <main className="grid grid-cols-6 lg:grid-cols-8 gap-4">{children}</main>
   );
