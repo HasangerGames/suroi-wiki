536a8262364b45433fcf4ec23e500666fe345707
Compositr <compositr@compositr.dev>
2023-11-05 22:59:43 +1100

    feat: Listing of all weapons

---
+++ app/(wiki)/weapons/page.tsx
@@ -0,0 +1,66 @@
+import { Guns } from "@/vendor/suroi/common/src/definitions/guns";
+import { Melees } from "@/vendor/suroi/common/src/definitions/melees";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function WeaponsPage() {
+  return (
+    <main className="text-white">
+      <div className="prose prose-invert">
+        <h1>Weapons</h1>
+        <p>
+          There are currently {Guns.length} guns in the game, and{" "}
+          {Melees.length} melee weapons in the game.
+        </p>
+      </div>
+      <WeaponsFlexTable>
+        {Guns.map((gun) => (
+          <div key={gun.idString} className="flex divide-x divide-muted">
+            <div className="flex p-2 items-center justify-center">
+              <Image
+                src={`https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/weapons/${gun.idString}.svg`}
+                width={128}
+                height={128}
+                alt={`Image of ${gun.name}`}
+              />
+            </div>
+            <div className="flex-1 flex items-center p-2">
+              <Link href={`/weapons/guns/${gun.idString}`}>
+                <h2 className="text-lg font-bold underline">{gun.name}</h2>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </WeaponsFlexTable>
+      <WeaponsFlexTable>
+        {Melees.map((melee) => (
+          <div key={melee.idString} className="flex divide-x divide-muted">
+            <div className="flex p-2 items-center justify-center">
+              <Image
+                src={`https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/weapons/${melee.idString}.svg`}
+                width={128}
+                height={128}
+                alt={`Image of ${melee.name}`}
+              />
+            </div>
+            <div className="flex-1 flex items-center p-2">
+              <Link href={`/weapons/melee/${melee.idString}`}>
+                <h2 className="text-lg font-bold underline">{melee.name}</h2>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </WeaponsFlexTable>
+    </main>
+  );
+}
+
+function WeaponsFlexTable({ children }: React.PropsWithChildren) {
+  return (
+    <div className="mt-8 flex justify-center">
+      <div className="w-[60ch] flex divide-y divide-muted flex-col border border-muted">
+        {children}
+      </div>
+    </div>
+  );
+}
