36e7376ec715c7133c60dc93c147ce48aee2269c
Compositr <compositr@compositr.dev>
2023-11-06 19:05:55 +1100

    chore: lint

--- app/(wiki)/(articles)/layout.tsx
+++ app/(wiki)/(articles)/layout.tsx
@@ -1,5 +1,7 @@
 export default function ArticleLayout({ children }: React.PropsWithChildren) {
   return (
-    <main className="sm:grid flex flex-col-reverse sm:grid-cols-6 lg:grid-cols-8 gap-4">{children}</main>
+    <main className="sm:grid flex flex-col-reverse sm:grid-cols-6 lg:grid-cols-8 gap-4">
+      {children}
+    </main>
   );
 }
