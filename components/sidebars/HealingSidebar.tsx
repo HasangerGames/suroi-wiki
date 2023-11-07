6f3cbc529ba24b66a93aede1195c2056988130ea
Compositr <compositr@compositr.dev>
2023-11-07 17:03:37 +1100

    feat: capacity stats for healing items - remove verb

--- components/sidebars/HealingSidebar.tsx
+++ components/sidebars/HealingSidebar.tsx
@@ -8,6 +8,7 @@ import InfoxboxHeader from "./utils/InfoboxHeader";
 import InfoboxRow from "./utils/InfoboxRow";
 import InfoboxAudioGroup from "./utils/InfoboxAudioGroup";
 import InfoboxAudio from "./utils/InfoboxAudio";
+import { Backpacks } from "@/vendor/suroi/common/src/definitions/backpacks";
 
 export default function HealingSidebar({
   item,
@@ -19,32 +20,39 @@ export default function HealingSidebar({
       title={item.name}
       image={`https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/loot/${item.idString}.svg`}
     >
-      <InfoboxRow grid="grid-cols-2">
+      <InfoboxRow>
         <InfoboxColumn title="Restores">
           {item.restoreAmount}
           {item.healType === HealType.Adrenaline && "%"}{" "}
           {HealType[item.healType]}
         </InfoboxColumn>
-        <InfoboxColumn title="Verb">
-          {item.useText} {item.name}
-        </InfoboxColumn>
-      </InfoboxRow>
-      <InfoboxRow grid="grid-cols-2">
         <InfoboxColumn title="Use Time" abbr="Time it takes to use this item">
           {item.useTime}s
         </InfoboxColumn>
         <InfoboxColumn
           title="Healing Per Second"
-          abbr="Amount of healing done per second. This is a useless stat."
+          abbr="Amount of healing done per second"
         >
           {(item.restoreAmount / item.useTime).toFixed(2)}
           {item.healType === HealType.Adrenaline && "%"}
         </InfoboxColumn>
       </InfoboxRow>
 
+      <InfoxboxHeader>Capacity</InfoxboxHeader>
+      <InfoboxRow grid="grid-cols-4">
+        {Backpacks.map((backpack) => (
+          <InfoboxColumn title={backpack.name} key={backpack.idString}>
+            {backpack.maxCapacity[item.idString]}
+          </InfoboxColumn>
+        ))}
+      </InfoboxRow>
+
       <InfoxboxHeader>Sounds</InfoxboxHeader>
       <InfoboxAudioGroup>
-        <InfoboxAudio name="Use" src={`https://github.com/HasangerGames/suroi/raw/master/client/public/audio/sfx/healing/${item.idString}.mp3`}/>
+        <InfoboxAudio
+          name="Use"
+          src={`https://github.com/HasangerGames/suroi/raw/master/client/public/audio/sfx/healing/${item.idString}.mp3`}
+        />
       </InfoboxAudioGroup>
 
       <InfoxboxHeader>Advanced Stats</InfoxboxHeader>
