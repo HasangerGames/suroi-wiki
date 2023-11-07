adf4ae41075bf2e8e85553f84b89dd94bc1bb6ba
Compositr <compositr@compositr.dev>
2023-11-07 16:44:46 +1100

    feat: healing items article page

--- app/(wiki)/(articles)/healing/[item]/page.tsx
+++ app/(wiki)/(articles)/healing/[item]/page.tsx
@@ -1,4 +1,23 @@
-export default function HealingPage() {
-  // TODO: Article MDX functionality 
-  return <></>;
+import { HealingItems } from "@/vendor/suroi/common/src/definitions/healingItems";
+import { notFound } from "next/navigation";
+import GauzeArticle from "../articles/gauze.mdx";
+import { MDXProps } from "mdx/types";
+
+export function generateMetadata({ params }: { params: { item: string } }) {
+  const item = HealingItems.find((item) => item.idString === params.item);
+  if (!item) notFound();
+
+  return {
+    title: item.name,
+  };
+}
+
+const ARTICLES = {
+  gauze: GauzeArticle,
+} as Record<string, (props: MDXProps) => JSX.Element>;
+
+export default function HealingPage({ params }: { params: { item: string } }) {
+  // TODO: Article MDX functionality
+  const article = ARTICLES[params.item] ?? null;
+  return <>{article?.({})}</>;
 }
