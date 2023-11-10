670849c91c5659f8c2920fbab2d41c37725bae1b
Compositr <compositr@compositr.dev>
2023-11-10 17:26:20 +1100

    feat: new articles - ak47 - s_g17 - usas12

--- app/(wiki)/(articles)/weapons/guns/[gun]/page.tsx
+++ app/(wiki)/(articles)/weapons/guns/[gun]/page.tsx
@@ -1,6 +1,8 @@
 import { Guns } from "@/vendor/suroi/common/src/definitions/guns";
 import { notFound } from "next/navigation";
 import AK47Article from "../articles/ak47.mdx";
+import USAS12Article from "../articles/usas12.mdx"
+import SG17Article from "../articles/s_g17.mdx"
 import { MDXProps } from "mdx/types";
 
 export function generateMetadata({ params }: { params: { gun: string } }) {
@@ -20,6 +22,8 @@ export function generateStaticParams() {
 
 const ARTICLES = {
   ak47: AK47Article,
+  usas12: USAS12Article,
+  s_g17: SG17Article
 } as Record<string, (props: MDXProps) => JSX.Element>;
 
 export default function GunPage({ params }: { params: { gun: string } }) {
