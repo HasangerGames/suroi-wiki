84f61a5bd759541d8897579f5502a641e59032ab
Compositr <compositr@compositr.dev>
2023-11-12 18:19:32 +1100

    feat: more generic function factories

--- app/(wiki)/(articles)/equipment/backpacks/[item]/page.tsx
+++ app/(wiki)/(articles)/equipment/backpacks/[item]/page.tsx
@@ -1,7 +1,14 @@
-import GenericMDXPageFactory from "@/components/layouts/GenericMDXPageFactory";
+import GenericMDXPageFactory, {
+  GenericGenerateMetadataFactory,
+  GenericGenerateStaticParamsFactory,
+} from "@/components/layouts/GenericMDXPageFactory";
 import Pack2Article from "../articles/pack_2.mdx";
 import Pack3Article from "../articles/pack_3.mdx";
 import Pack0Article from "../articles/pack_0.mdx";
+import { Backpacks } from "@/vendor/suroi/common/src/definitions/backpacks";
+
+export const generateMetadata = GenericGenerateMetadataFactory(Backpacks);
+export const generateStaticParams = GenericGenerateStaticParamsFactory(Backpacks);
 
 export default GenericMDXPageFactory({
   articles: {
--- components/layouts/GenericMDXPageFactory.tsx
+++ components/layouts/GenericMDXPageFactory.tsx
@@ -1,4 +1,6 @@
+import { ItemDefinition } from "@/vendor/suroi/common/src/utils/objectDefinitions";
 import { MDXProps } from "mdx/types";
+import { notFound } from "next/navigation";
 
 export default function GenericMDXPageFactory(args: GenericMDXPageFactoryArgs) {
   return function GenericMDXPage({
@@ -15,4 +17,25 @@ export default function GenericMDXPageFactory(args: GenericMDXPageFactoryArgs) {
 
 export interface GenericMDXPageFactoryArgs {
   articles: Record<string, (props: MDXProps) => JSX.Element>;
+}
+
+export function GenericGenerateStaticParamsFactory<T extends ItemDefinition>(
+  items: T[]
+) {
+  return function () {
+    return items.map((item) => ({
+      item: item.idString,
+    }));
+  };
+}
+
+export function GenericGenerateMetadataFactory<T extends ItemDefinition>(items: T[]) {
+  return function ({ params }: { params: { item: string } }) {
+    const item = items.find((item) => item.idString === params.item);
+    if (!item) notFound();
+
+    return {
+      title: item.name,
+    };
+  };
 }
