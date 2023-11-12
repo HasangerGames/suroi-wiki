de051c67e6cf5c0d02e4b2f2d2b2ded883a404c4
Compositr <compositr@compositr.dev>
2023-11-12 15:38:53 +1100

    fix: spacing on mobile

--- components/articles/notices/Event.tsx
+++ components/articles/notices/Event.tsx
@@ -2,8 +2,8 @@ import Emote from "@/components/icons/Emote";
 
 export default function EventItem() {
   return (
-    <div className="p-2 pl-4 bg-muted flex items-center">
-      <div className="ml-2 flex items-center">
+    <div className="p-2 pl-4 bg-muted flex gap-4 items-center">
+      <div className="flex items-center">
         <Emote emote="happy_face" />
       </div>
       <div className="flex-1 flex justify-center">
