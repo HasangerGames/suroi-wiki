c4087f370d6664a22e8119a2387e3459b2ef2199
Compositr <compositr@compositr.dev>
2023-11-13 09:59:29 +1100

    feat: client TZ TimeLink

--- components/links/TimeLink.tsx
+++ components/links/TimeLink.tsx
@@ -1,1 +1,28 @@
-export default function TimeLink() {}
+"use client";
+
+import { DateTime } from "luxon";
+import { Tooltip } from "react-tooltip";
+
+export default function TimeLink({ time }: TimeLinkProps) {
+  const dateTime = DateTime.fromMillis(time);
+  return (
+    <>
+      <span
+        data-tooltip-id="time"
+        data-tooltip-content={dateTime.toString()}
+        className="hover:cursor-pointer p-2 mx-1 rounded-md bg-muted"
+        onClick={() => navigator.clipboard.writeText(dateTime.toString())}
+      >
+        {dateTime.toLocaleString({
+          timeStyle: "medium",
+          dateStyle: "medium"
+        })}
+      </span>
+      <Tooltip id="time" />
+    </>
+  );
+}
+
+export interface TimeLinkProps {
+  time: number;
+}
