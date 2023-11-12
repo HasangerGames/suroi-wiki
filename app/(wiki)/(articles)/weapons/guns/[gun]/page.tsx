f3c0f8cb7f3013182700fc11ef17dd20a02777b7
Compositr <compositr@compositr.dev>
2023-11-12 21:34:47 +1100

    feat: Lewis Gun article

--- app/(wiki)/(articles)/weapons/guns/[gun]/page.tsx
+++ app/(wiki)/(articles)/weapons/guns/[gun]/page.tsx
@@ -5,6 +5,7 @@ import USAS12Article from "../articles/usas12.mdx";
 import SG17Article from "../articles/s_g17.mdx";
 import HP18Article from "../articles/hp18.mdx";
 import Stoner63Article from "../articles/stoner_63.mdx";
+import LewisGunArticle from "../articles/lewis_gun.mdx";
 import { MDXProps } from "mdx/types";
 
 export function generateMetadata({ params }: { params: { gun: string } }) {
@@ -28,6 +29,7 @@ const ARTICLES = {
   s_g17: SG17Article,
   hp18: HP18Article,
   stoner_63: Stoner63Article,
+  lewis_gun: LewisGunArticle,
 } as Record<string, (props: MDXProps) => JSX.Element>;
 
 export default function GunPage({ params }: { params: { gun: string } }) {
