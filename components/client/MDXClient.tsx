b75f0a8792270c1f30ad15d36c548b94ada00a2d
Compositr <compositr@compositr.dev>
2023-11-13 20:41:01 +1100

    fix: provide CommitLink and FileLink

--- components/client/MDXClient.tsx
+++ components/client/MDXClient.tsx
@@ -4,11 +4,15 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
 import Link from "next/link";
 import EventItem from "../articles/notices/Event";
 import TimeLink from "../links/TimeLink";
+import CommitLink from "../links/CommitLink";
+import FileLink from "../links/FileLink";
 
 const components = {
   Link,
   Event: EventItem,
   TimeLink,
+  FileLink,
+  CommitLink
 };
 
 export default function MDXClient(props: MDXRemoteProps) {
