1067d5b680717b393b861bd40f9f22dae500d2e1
Compositr <compositr@compositr.dev>
2023-11-14 16:50:01 +1100

    refactor: create GenericNotice element

--- components/articles/notices/Event.tsx
+++ components/articles/notices/Event.tsx
@@ -1,17 +1,10 @@
-import Emote from "@/components/icons/Emote";
+import GenericNotice from "./GenericNotice";
 
 export default function EventItem() {
   return (
-    <div className="p-2 pl-4 bg-muted flex gap-4 items-center">
-      <div className="flex items-center">
-        <Emote emote="happy_face" />
-      </div>
-      <div className="flex-1 flex justify-center">
-        <span className="font-bold">
-          This article is about gameplay or items only available during certain
-          Events.
-        </span>
-      </div>
-    </div>
+    <GenericNotice emote="happy_face">
+      This article is about gameplay or items only available during certain
+      Events.
+    </GenericNotice>
   );
 }
