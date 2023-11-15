9007d12ad1553baa77369bd7abe7581d71779dba
Compositr <compositr@compositr.dev>
2023-11-15 15:11:29 +1100

    fix: audio not showing on chromium

--- components/sidebars/utils/InfoboxAudio.tsx
+++ components/sidebars/utils/InfoboxAudio.tsx
@@ -2,7 +2,7 @@ export default function InfoboxAudio({ name, src }: InfoboxAudioProps) {
   return (
     <div className="p-2 flex flex-row gap-2 items-center">
       <h4 className="font-bold mr-2">{name}</h4>
-      <audio controls className="w-full h-full">
+      <audio controls className="w-full">
         <source src={src} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
