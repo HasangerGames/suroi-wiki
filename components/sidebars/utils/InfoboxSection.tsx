8ff46084b10f139fe94262b73a61c3991665c3f6
Compositr <compositr@compositr.dev>
2023-11-15 20:46:09 +1100

    wip: refactoring the revit sidebar

--- components/sidebars/utils/InfoboxSection.tsx
+++ components/sidebars/utils/InfoboxSection.tsx
@@ -11,22 +11,23 @@ export default function InfoboxSection({
   children,
 }: InfoboxSectionProps) {
   const [isOpened, setIsOpened] = useState(true);
-  
+
   return (
     <>
-      <div
-        onClick={() => setIsOpened(!isOpened)}
-        className="relative"
-      >
-        <div className="p-2 bg-secondary">
-          <h3 className="font-bold text-center">{abbr ? <abbr title={abbr}>{title}</abbr> : title}</h3>
+      <div onClick={() => setIsOpened(!isOpened)} className="relative cursor-pointer">
+        <div className="p-2 bg-primary">
+          <h3 className="font-bold text-center">
+            {abbr ? <abbr title={abbr}>{title}</abbr> : title}
+          </h3>
         </div>
         <div className="absolute right-2 top-1/4">
           {isOpened ? <ChevronUp /> : <ChevronDown />}
         </div>
       </div>
       <Collapse isOpened={isOpened}>
-        <div className={`border-t border-t-primary text-center divide-x divide-primary grid ${grid} text-sm`}>
+        <div
+          className={`border-t border-t-primary text-center divide-x divide-primary grid ${grid} text-sm`}
+        >
           {children}
         </div>
       </Collapse>
@@ -35,7 +36,7 @@ export default function InfoboxSection({
 }
 
 export interface InfoboxSectionProps extends React.PropsWithChildren {
-  grid?: `grid-cols-${number}`
+  grid?: `grid-cols-${number}`;
   abbr?: string;
   title?: string;
 }
