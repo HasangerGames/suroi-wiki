8a89afa89fd507767e968bbf38b89f2ad2c70a6e
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-17 21:22:57 -0700

    Add stub notice

---
+++ components/articles/notices/Stub.tsx
@@ -0,0 +1,9 @@
+import GenericNotice from "./GenericNotice";
+
+export default function Removed() {
+  return (
+    <GenericNotice emote="thinking_face">
+      This article is a stub, meaning the content is not complete (but the statistics are)
+    </GenericNotice>
+  );
+}
