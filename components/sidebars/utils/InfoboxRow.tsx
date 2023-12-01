66e87b91bae258132b1935475bba14e95f1a5191
Compositr <compositr@compositr.dev>
2023-12-01 23:43:21 +1100

    fix: properly count children that are not there

--- components/sidebars/utils/InfoboxRow.tsx
+++ components/sidebars/utils/InfoboxRow.tsx
@@ -1,12 +1,17 @@
 import { Children } from "react";
 
 export default function InfoboxRow({ columns, children }: InfoboxRowProps) {
+  let childrenCount = 0;
+  Children.forEach(children, (child) =>
+    Boolean(child) ? childrenCount++ : null
+  );
+  
   return (
     <div
       className={`border-t-primary border-t text-center divide-x divide-primary grid text-sm`}
       style={{
         gridTemplateColumns: `repeat(${
-          columns ?? Children.count(children)
+          columns ?? childrenCount
         }, minmax(0, 1fr))`,
       }}
     >
