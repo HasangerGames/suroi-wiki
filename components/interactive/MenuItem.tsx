a73c7d2282ed493b84da50ee785fc9c7467871e4
Compositr <compositr@compositr.dev>
2023-12-14 23:31:28 +1100

    feat: close mobile nav on change

--- components/interactive/MenuItem.tsx
+++ components/interactive/MenuItem.tsx
@@ -2,15 +2,16 @@
 
 import Link from "next/link";
 
-export default function MenuItem({ title, href }: MenuItemProps) {
+export default function MenuItem({ title, href, onClick }: MenuItemProps) {
   return (
     <div className="text-lg font-bold leading-loose underline transition-colors hover:text-blue-500">
-      <Link href={href}>{title}</Link>
+      <Link href={href} onClick={onClick}>{title}</Link>
     </div>
   );
 }
 
 export interface MenuItemProps extends React.PropsWithChildren {
   title: string,
-  href: string
+  href: string,
+  onClick?: () => unknown,
 }
