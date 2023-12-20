0589d16aeb343b9673b612339d00376d2388fcdd
Compositr <compositr@compositr.dev>
2023-12-20 14:26:52 +1100

    feat: dual gun switcher mechanism

--- components/sidebars/GunSidebar.tsx
+++ components/sidebars/GunSidebar.tsx
@@ -1,424 +1,47 @@
-import { FireMode } from "@/vendor/suroi/common/src/constants";
+"use client";
+
 import {
   GunDefinition,
   Guns,
 } from "@/vendor/suroi/common/src/definitions/guns";
-import InfoboxRow from "./utils/InfoboxRow";
-import InfoboxColumn from "./utils/InfoboxColumn";
-import AmmoIcon from "../icons/AmmoIcon";
-import InfoboxHeader from "./utils/InfoboxHeader";
 import { ExplosionDefinition } from "@/vendor/suroi/common/src/definitions/explosions";
 import GenericSidebar from "./utils/GenericSidebar";
-import InfoboxAudio from "./utils/InfoboxAudio";
-import InfoboxAudioGroup from "./utils/InfoboxAudioGroup";
-import InfoboxSection from "./utils/InfoboxSection";
-import { Unpacked, WithRequired } from "@/lib/ts/utility";
-import {
-  ItemDefinition,
-  WearerAttributes,
-} from "@/vendor/suroi/common/src/utils/objectDefinitions";
-import {
-  getSuroiImageLink,
-  getSuroiItem,
-  getSuroiKillfeedImageLink,
-} from "@/lib/util/suroi";
-import Image from "next/image";
-import Link from "../links/Link";
+import { getSuroiImageLink } from "@/lib/util/suroi";
+import GunDetails from "./GunDetails";
+import { useState } from "react";
 
 export default function GunSidebar({ gun, explosion }: GunSidebarProps) {
+  const [dual, setDual] = useState(false);
+  const dualGun = Guns.find((g) => g.idString === `dual_${gun.idString}`);
   return (
-    <GenericSidebar
-      title={gun.name}
-      image={getSuroiImageLink(gun, undefined, undefined, true)}
-    >
-      <InfoboxRow>
-        <InfoboxColumn title="World Image">
-          <div className="flex items-center">
-            <Image
-              width={128}
-              height={128}
-              alt={`World image of ${gun.name}`}
-              src={getSuroiImageLink(
-                gun.isDual ? getSuroiItem(gun.singleVariant)! : gun,
-                undefined,
-                "world"
-              )}
-            />
-          </div>
-        </InfoboxColumn>
-        <InfoboxColumn title="Killfeed Icon">
-          <Image
-            width={128}
-            height={128}
-            alt={`Killfeed icon of ${gun.name}`}
-            src={getSuroiKillfeedImageLink(gun)}
-          />
-        </InfoboxColumn>
-      </InfoboxRow>
-      <InfoboxRow>
-        <InfoboxColumn title="Fire Mode">
-          {FireMode[gun.fireMode]}
-        </InfoboxColumn>
-        <InfoboxColumn title="Ammo Type">
-          <AmmoIcon ammo={gun.ammoType} scale={0.5} />
-        </InfoboxColumn>
-        <InfoboxColumn title="Capacity">{gun.capacity}</InfoboxColumn>
-      </InfoboxRow>
-      <InfoboxRow>
-        <InfoboxColumn title="Reload">
-          {gun.singleReload ? "1" : gun.capacity} in {gun.reloadTime}s
-        </InfoboxColumn>
-        <InfoboxColumn title="Firing Delay" abbr="Delay between shots">
-          {gun.fireDelay}ms
-        </InfoboxColumn>
-        <InfoboxColumn
-          title="Switch Delay"
-          abbr="Cooldown between switching between this weapon and another"
-        >
-          {gun.switchDelay}ms
-        </InfoboxColumn>
-      </InfoboxRow>
-
-      <InfoboxRow>
-        <InfoboxColumn
-          title="Spread"
-          abbr="Spread (angle of inaccuracy) when still"
-        >
-          {gun.shotSpread}°
-        </InfoboxColumn>
-        {gun.isDual && (
-          <InfoboxColumn title="Single Variant">
-            <Link href={`/weapons/guns/${gun.singleVariant}`}>
-              {getSuroiItem(gun.singleVariant)?.name}
-            </Link>
-          </InfoboxColumn>
-        )}
-        {[Guns.find((g) => g.idString === `dual_${gun.idString}`)]
-          .filter(Boolean)
-          .map((gun) => (
-            <InfoboxColumn title="Dual Variant" key={gun?.idString}>
-              <Link href={`/weapons/guns/dual_${gun?.idString}`}>
-                {gun?.name}
-              </Link>
-            </InfoboxColumn>
-          ))}
-        <InfoboxColumn
-          title="Move Spread"
-          abbr="Spread (angle of inaccuracy when moving)"
-        >
-          {gun.moveSpread}°{" "}
-          <abbr title="When compared to normal spread">
-            ({gun.moveSpread - gun.shotSpread > 0 ? "+" : ""}
-            {(gun.moveSpread - gun.shotSpread).toFixed(2)}°)
-          </abbr>
-        </InfoboxColumn>
-      </InfoboxRow>
-      <InfoboxHeader>Ballistics</InfoboxHeader>
-      <InfoboxRow>
-        <InfoboxColumn title="Damage">{gun.ballistics.damage}</InfoboxColumn>
-        <InfoboxColumn title="Bullet Speed">
-          {gun.ballistics.speed}
-        </InfoboxColumn>
-        <InfoboxColumn title="Range">{gun.ballistics.range}</InfoboxColumn>
-      </InfoboxRow>
-
-      <InfoboxRow>
-        <InfoboxColumn title="Obstacle Damage">
-          x{gun.ballistics.obstacleMultiplier}
-        </InfoboxColumn>
-        <InfoboxColumn
-          title="Max. DPS"
-          abbr="Hypothetical maximum damage per second (DPS)"
-        >
-          {gun.fireMode === FireMode.Burst &&
-            (
-              (1000 /
-                (gun.burstProperties.burstCooldown +
-                  gun.fireDelay * gun.burstProperties.shotsPerBurst)) *
-              (gun.ballistics.damage * gun.burstProperties.shotsPerBurst)
-            ).toFixed(2)}
-          {gun.fireMode !== FireMode.Burst &&
-            (
-              gun.ballistics.damage *
-              (gun.bulletCount ?? 1) *
-              (1000 / gun.fireDelay)
-            ).toFixed(2)}
-        </InfoboxColumn>
-        <InfoboxColumn
-          title="Max. Obstacle DPS"
-          abbr="Hypothetical maximum damage per second (DPS) on obstacles"
-        >
-          {gun.fireMode === FireMode.Burst &&
-            (
-              (1000 /
-                (gun.burstProperties.burstCooldown +
-                  gun.fireDelay * gun.burstProperties.shotsPerBurst)) *
-              (gun.ballistics.damage *
-                gun.ballistics.obstacleMultiplier *
-                gun.burstProperties.shotsPerBurst)
-            ).toFixed(2)}
-          {gun.fireMode !== FireMode.Burst &&
-            (
-              gun.ballistics.damage *
-              gun.ballistics.obstacleMultiplier *
-              (gun.bulletCount ?? 1) *
-              (1000 / gun.fireDelay)
-            ).toFixed(2)}
-        </InfoboxColumn>
-      </InfoboxRow>
-
-      {gun.bulletCount && (
-        <>
-          <InfoboxHeader>Shotgun Stats</InfoboxHeader>
-          <InfoboxRow>
-            <InfoboxColumn title="Bullet Count" abbr="Bullets fired per shot">
-              {gun.bulletCount}
-            </InfoboxColumn>
-            <InfoboxColumn title="Jitter Radius" abbr="Random bullet offset">
-              {gun.jitterRadius ?? "None"}
-            </InfoboxColumn>
-            <InfoboxColumn
-              title="Total Damage"
-              abbr="Total damage per shot (bullets x damage per bullet)"
-            >
-              {gun.bulletCount * gun.ballistics.damage}
-            </InfoboxColumn>
-          </InfoboxRow>
-        </>
+    <div className="col-span-2">
+      {dualGun && (
+        <div className="mb-2 flex gap-2 text-lg">
+          <button onClick={() => setDual(false)} className={`${!dual && "text-primary underline"} transition-colors`}>Single</button>
+          <button
+            onClick={() => setDual(true)}
+            className={`${dual && "text-primary underline"} transition-colors`}
+          >
+            Dual
+          </button>
+        </div>
       )}
-      
-      {gun.wearerAttributes && <Effects gun={gun as any} />}
-
-      {explosion && (
-        <>
-          <InfoboxHeader>Explosion Ballistics</InfoboxHeader>
-          <InfoboxRow>
-            <InfoboxColumn title="Explosion Damage">
-              {explosion.damage}
-            </InfoboxColumn>
-            <InfoboxColumn title="Explosion Obstacle Damage">
-              x{explosion.obstacleMultiplier}
-            </InfoboxColumn>
-            <InfoboxColumn title="Explosion Radius">
-              Min: {explosion.radius.min} Max: {explosion.radius.max}
-            </InfoboxColumn>
-          </InfoboxRow>
-          <InfoboxRow>
-            <InfoboxColumn title="Shrapnel Count">
-              {explosion.shrapnelCount}
-            </InfoboxColumn>
-            <InfoboxColumn title="Shrapnel Damage">
-              {explosion.ballistics.damage}
-            </InfoboxColumn>
-            <InfoboxColumn title="Shrapnel Speed">
-              {explosion.ballistics.speed}
-            </InfoboxColumn>
-          </InfoboxRow>
-          <InfoboxRow>
-            <InfoboxColumn title="Shrapnel Range">
-              {explosion.ballistics.range}
-            </InfoboxColumn>
-            <InfoboxColumn title="Shrapnel Obstacle Damage">
-              x{explosion.ballistics.obstacleMultiplier}
-            </InfoboxColumn>
-            <InfoboxColumn
-              title="Shrapnel Total Damage"
-              abbr="Damage of all shrapnel pieces"
-            >
-              {explosion.shrapnelCount * explosion.ballistics.damage}
-            </InfoboxColumn>
-          </InfoboxRow>
-        </>
-      )}
-
-      <InfoboxAudioGroup>
-        <InfoboxAudio
-          name="Fire"
-          src={`https://github.com/HasangerGames/suroi/raw/master/client/public/audio/sfx/weapons/${gun.idString.replace(
-            "dual_",
-            ""
-          )}_fire.mp3`}
-        />
-        <InfoboxAudio
-          name="Switch"
-          src={`https://github.com/HasangerGames/suroi/raw/master/client/public/audio/sfx/weapons/${gun.idString.replace(
-            "dual_",
-            ""
-          )}_switch.mp3`}
-        />
-        <InfoboxAudio
-          name="Reload"
-          src={`https://github.com/HasangerGames/suroi/raw/master/client/public/audio/sfx/weapons/${gun.idString}_reload.mp3`}
-        />
-        {explosion && (
-          <InfoboxAudio
-            name="Explosion"
-            // HACK: hardcoded because USAS is the only explosive gun atm
-            src={`https://github.com/HasangerGames/suroi/raw/master/client/public/audio/sfx/${explosion.idString}.mp3`}
-          />
+      <GenericSidebar
+        title={dual ? dualGun?.name ?? gun.name : gun.name}
+        image={getSuroiImageLink(
+          dual ? dualGun! : gun,
+          undefined,
+          undefined,
+          true
         )}
-      </InfoboxAudioGroup>
-
-      <InfoboxHeader>Advanced Stats</InfoboxHeader>
-      <InfoboxRow>
-        <InfoboxColumn title="Internal ID">
-          <span className="font-mono">{gun.idString}</span>
-        </InfoboxColumn>
-      </InfoboxRow>
-    </GenericSidebar>
+      >
+        <GunDetails gun={dual ? dualGun! : gun} explosion={explosion} />
+      </GenericSidebar>
+    </div>
   );
 }
 
 export interface GunSidebarProps {
   gun: GunDefinition;
   explosion?: ExplosionDefinition;
-}
-
-function Effects({
-  gun,
-}: {
-  gun: WithRequired<GunDefinition, "wearerAttributes">;
-}) {
-  return (
-    <>
-      <InfoboxHeader>Effects</InfoboxHeader>
-      {gun.wearerAttributes.passive && (
-        <>
-          <InfoboxRow>
-            <InfoboxHeader>Passive</InfoboxHeader>
-          </InfoboxRow>
-          <InfoboxRow>
-            <Attributes attributes={gun.wearerAttributes.passive} />
-          </InfoboxRow>
-        </>
-      )}
-
-      {gun.wearerAttributes.on && (
-        <>
-          <InfoboxRow>
-            {gun.wearerAttributes.on.kill && (
-              <>
-                <InfoboxHeader>On Kill</InfoboxHeader>
-                {gun.wearerAttributes.on.kill.map((attr, i) => (
-                  <Attributes attributes={attr} key={i} n={i} />
-                ))}
-              </>
-            )}
-          </InfoboxRow>
-
-          <InfoboxRow>
-            {gun.wearerAttributes.on.damageDealt && (
-              <>
-                <InfoboxHeader>On Damage Dealt</InfoboxHeader>
-                {gun.wearerAttributes.on.damageDealt.map((attr, i) => (
-                  <Attributes attributes={attr} key={i} n={i} />
-                ))}
-              </>
-            )}
-          </InfoboxRow>
-        </>
-      )}
-    </>
-  );
-}
-
-function Attributes({
-  attributes,
-  n,
-}: {
-  attributes:
-    | WearerAttributes
-    | NonNullable<
-        Unpacked<
-          WithRequired<
-            WithRequired<
-              ItemDefinition,
-              "wearerAttributes"
-            >["wearerAttributes"],
-            "on"
-          >["on"]["kill"]
-        >
-      >;
-  n?: number;
-}) {
-  const limit =
-    "limit" in attributes ? (
-      <>
-        {" "}
-        <abbr
-          title={`This effect can be applied up to a maximum of ${attributes.limit} times`}
-          className="inline-block ml-[1ch]"
-        >
-          ({attributes.limit} limit)
-        </abbr>
-      </>
-    ) : (
-      <>
-        {" "}
-        <abbr
-          title={`This effect can be applied an infinite amount of times`}
-          className="inline-block ml-[1ch]"
-        >
-          (No limit)
-        </abbr>
-      </>
-    );
-
-  return (
-    <>
-      {n !== undefined && (
-        <div className="border-t border-primary">
-          <InfoboxColumn title={`Effect ${n + 1}`}></InfoboxColumn>
-        </div>
-      )}
-      <InfoboxRow>
-        {attributes.maxHealth && (
-          <InfoboxColumn title="Health Multiplier">
-            {attributes.maxHealth}
-            {limit}
-          </InfoboxColumn>
-        )}
-        {attributes.maxAdrenaline && (
-          <InfoboxColumn title="Adrenaline Multiplier">
-            {attributes.maxAdrenaline ?? "None"}
-            {limit}
-          </InfoboxColumn>
-        )}
-      </InfoboxRow>
-      <InfoboxRow>
-        {attributes.minAdrenaline && (
-          <InfoboxColumn title="Min. Adrenaline">
-            {attributes.minAdrenaline ?? "Unchanged"}
-            {limit}
-          </InfoboxColumn>
-        )}
-        {attributes.speedBoost && (
-          <InfoboxColumn title="Speed Multipiler">
-            {attributes.speedBoost ?? "None"}
-            {limit}
-          </InfoboxColumn>
-        )}
-      </InfoboxRow>
-      <InfoboxRow>
-        {"healthRestored" in attributes && (
-          <InfoboxColumn
-            title="Health Restored"
-            abbr="Health restored when this condition is met"
-          >
-            {attributes.healthRestored}
-            {limit}
-          </InfoboxColumn>
-        )}
-        {"adrenalineRestored" in attributes && (
-          <InfoboxColumn
-            title="Adrenaline Restored"
-            abbr="Adrenaline restored when this condition is met"
-          >
-            {attributes.adrenalineRestored}
-            {limit}
-          </InfoboxColumn>
-        )}
-      </InfoboxRow>
-    </>
-  );
 }
