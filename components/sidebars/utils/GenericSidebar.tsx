15c66bcb39bc6ce3c466015e2d6292638c0b6d57
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-21 17:55:36 -0700

    feat: better tab styling and fix image sizing for sidebars

--- components/sidebars/utils/GenericSidebar.tsx
+++ components/sidebars/utils/GenericSidebar.tsx
@@ -18,22 +18,23 @@ export default function GenericSidebar({
         <div className="p-2 border-b bg-primary border-primary">
           <h2 className="text-xl font-bold text-center">{title}</h2>
         </div>
-        <div className="flex flex-col items-center justify-center gap-2 p-2">
-          {imageVariations?.length !== undefined && imageVariations.length !== 0 && (
-            <div className="flex w-full justify-around gap-4 items-center pb-1">
-              {imageVariations.map((image, i) => (
-                <button
-                  key={image}
-                  className={`cursor-pointer text-muted-foreground hover:text-white ${
-                    imageNum === i ? "!text-white underline" : ""
-                  } p-2 rounded-md border`}
-                  onClick={() => setImageNum(i)}
-                >
-                  {i + 1}
-                </button>
-              ))}
-            </div>
-          )}
+        <div className="flex flex-col items-center justify-start gap-2 p-2">
+          {imageVariations?.length !== undefined &&
+            imageVariations.length !== 0 && (
+              <div className="flex flex-row flex-wrap w-full justify-around gap-2 items-center p-1">
+                {imageVariations.map((image, i) => (
+                  <button
+                    key={image}
+                    className={`flex justify-center grow rounded-md min-w-[7ch] hover:bg-muted/50 cursor-pointer text-muted-foreground hover:text-white ${
+                      imageNum === i ? "!text-white bg-muted ring-primary ring" : ""
+                    } p-2`}
+                    onClick={() => setImageNum(i)}
+                  >
+                    {i + 1}
+                  </button>
+                ))}
+              </div>
+            )}
           {noImage || !image.length ? (
             <div className="m-4 flex justify-center">
               <span>(No image available)</span>
@@ -41,9 +42,14 @@ export default function GenericSidebar({
           ) : (
             <Image
               priority
-              src={imageVariations && imageVariations.length !== 0 ? imageVariations[imageNum] : image}
+              src={
+                imageVariations && imageVariations.length !== 0
+                  ? imageVariations[imageNum]
+                  : image
+              }
               width={128}
               height={128}
+              className="w-32 h-32"
               alt={`Image of ${title}`}
             />
           )}
