3a2e242b75025fb95a98969b420f284dbdfac2a8
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-22 10:47:57 -0700

    feat: gallery working now

--- app/(wiki)/(articles)/kitchen-sink/page.tsx
+++ app/(wiki)/(articles)/kitchen-sink/page.tsx
@@ -20,9 +20,11 @@ export default async function Kitchen() {
           {
             url: "https://images.unsplash.com/photo-1702928153872-5754d411b0f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             caption: "test",
+            author: "John Doe"
           },
           {
-            url: "https://images.unsplash.com/photo-1682687218982-6c508299e107?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "https://images.unsplash.com/photo-1682687218982-6c508299e107?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            author: "Jane Doe"
           }
         ]}
       />
--- components/articles/gallery/Gallery.tsx
+++ components/articles/gallery/Gallery.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { ImageIcon, Maximize2, X, Youtube } from "lucide-react";
+import {
+  ChevronRight,
+  ChevronLeft,
+  ImageIcon,
+  Maximize2,
+  X,
+  Youtube,
+} from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -26,11 +33,56 @@ export default function Gallery({ images }: GalleryProps) {
   return (
     <>
       {fullscreen && (
-        <div className="fixed z-50 inset-0 w-full h-full bg-black/80 p-4">
-          <button onClick={() => setFullscreen(false)}>
+        <div className="flex flex-col gap-8 fixed z-50 inset-0 w-full h-full bg-black/80 p-4">
+          <button className="flex flex-row gap-8" onClick={() => setFullscreen(false)}>
             <X className="w-12 h-12" />
+            <span className="text-xl my-auto">Gallery</span>
           </button>
-          <p>wip</p>
+          <div className="relative w-full h-full">
+            <button
+              className="absolute left-0 top-[50%] translate-y-[-50%] rounded-full p-8 hover:bg-muted"
+              onClick={() =>
+                setCurrentImage((currentImage - 1) < 0 ? images.length - 1 : currentImage - 1)
+              }
+            >
+              <ChevronLeft className="w-16 h-16" />
+            </button>
+            <button
+              className="absolute right-0 top-[50%] translate-y-[-50%] rounded-full p-8 hover:bg-muted"
+              onClick={() =>
+                setCurrentImage((currentImage + 1) % images.length)
+              }
+            >
+              <ChevronRight className="w-16 h-16" />
+            </button>
+            <a href={images[currentImage].url} className="cursor-zoom-in">
+              <Image
+                src={images[currentImage].url}
+                alt={
+                  (images[currentImage].caption &&
+                    images[currentImage].caption) ||
+                  "Image with no caption"
+                }
+                width={500}
+                height={500}
+                className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+              />
+            </a>
+          </div>
+          <div className="flex flex-row justify-center gap-8">
+            {images.map((image, i) => (
+              // eslint-disable-next-line react/jsx-key
+              <button onClick={() => setCurrentImage(i)} className="">
+                <Image
+                  src={image.url}
+                  alt={image.url}
+                  width={50}
+                  height={50}
+                  className={`${currentImage === i && "ring-primary ring"} rounded-md`}
+                />
+              </button>
+            ))}
+          </div>
         </div>
       )}
       <button
@@ -39,7 +91,7 @@ export default function Gallery({ images }: GalleryProps) {
       >
         {/* Stupid hack lol */}
         <div
-          style={{ "background-image": `url(${firstImage.url})` }}
+          style={{ backgroundImage: `url(${firstImage.url})` }}
           className="bg-cover bg-center w-full h-full rounded-md"
         >
           <div className="relative transition-all group-hover:backdrop-brightness-50 w-full h-full rounded-md">
