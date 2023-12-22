19b248b74f2564cb9c60b35ee0290c54a7d29ca1
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-21 21:22:27 -0700

    feat: Fullscreen toggle scaffolding

--- components/articles/gallery/Gallery.tsx
+++ components/articles/gallery/Gallery.tsx
@@ -1,5 +1,8 @@
-import { ImageIcon, Maximize2 } from "lucide-react";
+"use client";
+
+import { ImageIcon, Maximize2, X } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 export enum GalleryImageType {
   Image,
@@ -14,22 +17,40 @@ export type GalleryImage = {
 };
 
 export default function Gallery({ images }: GalleryProps) {
+  const [fullscreen, setFullscreen] = useState(false);
+
+  const [currentImage, setCurrentImage] = useState(0);
+
   const firstImage = images[0];
+
   return (
-    <div className="group block cursor-zoom-in overflow-hidden bg-muted rounded-md p-2 aspect-video w-64 h-auto">
-      <div
-        style={{ "background-image": `url(${firstImage.url})` }}
-        className="bg-cover bg-center w-full h-full rounded-md"
-      >
-        <div className="relative transition-all group-hover:backdrop-brightness-50 w-full h-full rounded-md">
-          <Maximize2 className="absolute flex group-hover:visible invisible w-12 h-12 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]" />
-          <span className="absolute flex flex-row gap-1 right-2 bottom-2">
-            <ImageIcon className="w-6 h-6" />
-            {images.length.toFixed(0)}
-          </span>
+    <>
+      {fullscreen && (
+        <div className="fixed z-50 inset-0 w-full h-full bg-black/80 p-4">
+          <button onClick={() => setFullscreen(false)}>
+            <X className="w-12 h-12" />
+          </button>
         </div>
-      </div>
-    </div>
+      ) || (
+        <button
+          onClick={() => setFullscreen(true)}
+          className="group block cursor-zoom-in overflow-hidden bg-muted rounded-md p-2 aspect-video w-full md:w-64 h-auto"
+        >
+          <div
+            style={{ "background-image": `url(${firstImage.url})` }}
+            className="bg-cover bg-center w-full h-full rounded-md"
+          >
+            <div className="relative transition-all group-hover:backdrop-brightness-50 w-full h-full rounded-md">
+              <Maximize2 className="absolute flex group-hover:visible invisible w-12 h-12 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]" />
+              <span className="absolute flex flex-row gap-1 right-2 bottom-2">
+                <ImageIcon className="w-6 h-6" />
+                {images.length.toFixed(0)}
+              </span>
+            </div>
+          </div>
+        </button>
+      )}
+    </>
   );
 }
 
