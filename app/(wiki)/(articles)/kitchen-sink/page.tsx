dd1a42f37739e883fe46cc6b647460d9e6514e9f
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-21 22:13:05 -0700

    Enough React for today

--- app/(wiki)/(articles)/kitchen-sink/page.tsx
+++ app/(wiki)/(articles)/kitchen-sink/page.tsx
@@ -1,6 +1,6 @@
 // A component testing page
 
-import Gallery from "@/components/articles/gallery/Gallery";
+import Gallery, { GalleryImageType } from "@/components/articles/gallery/Gallery";
 import DevWeapon from "@/components/articles/notices/DevWeapon";
 import Empty from "@/components/articles/notices/Empty";
 import Event from "@/components/articles/notices/Event";
@@ -21,6 +21,9 @@ export default async function Kitchen() {
             url: "https://images.unsplash.com/photo-1702928153872-5754d411b0f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             caption: "test",
           },
+          {
+            url: "https://images.unsplash.com/photo-1682687218982-6c508299e107?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
         ]}
       />
     </div>
