f89950540e900bfbac0ea8020cd891687ae2f430
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-21 20:41:45 -0700

    feat: Gallery thumbnail

--- app/(wiki)/(articles)/kitchen-sink/page.tsx
+++ app/(wiki)/(articles)/kitchen-sink/page.tsx
@@ -15,7 +15,14 @@ export default async function Kitchen() {
       <Event />
       <Removed />
       <Stub />
-      <Gallery />
+      <Gallery
+        images={[
+          {
+            url: "https://images.unsplash.com/photo-1702928153872-5754d411b0f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            caption: "test",
+          },
+        ]}
+      />
     </div>
-  )
+  );
 }
