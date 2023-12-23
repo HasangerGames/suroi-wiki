0a3abaf9c70fa07111a5ec8b26bc3c7d50ee51f6
CroissantEdit <pureblu3d@gmail.com>
2023-12-22 22:09:55 -0800

    clickable suroi button
    dont ask me what i did

--- app/(wiki)/page.tsx
+++ app/(wiki)/page.tsx
@@ -11,68 +11,56 @@ export default async function Home() {
   const HEAD = await fs.readFile(".git/modules/vendor/suroi/HEAD", "utf8");
 
   return (
-    <main className="container mx-auto p-4">
-      <div className="prose prose-invert text-center">
-        <h1 className="text-5xl font-extrabold mb-4">Official Suroi Wiki</h1>
-        <div className="flex justify-center mb-8">
-          <a href="https://suroi.io" target="_blank">
-            <Image
-              src="/img/suroi.svg"
-              alt="Suroi Battle Royale"
-              width={1500 / 2}
-              height={350 / 2}
-              priority
-            />
+    <main>
+      <div className="prose prose-invert">
+        <h1>Official Suroi Wiki</h1>
+        <div className="flex justify-center">
+         <a href="https://suroi.io" target="_blank">
+          <Image
+            src="/img/suroi.svg"
+            alt="Suroi Battle Royale"
+            width={1350 / 1.5}
+            height={450 / 1.5}
+            priority
+          />
           </a>
         </div>
-        <div className="text-left mb-4"> {/* Adjusted to left-align text */}
-          <p className="text-lg">
-            Welcome to the official Suroi wiki! Suroi is an open-source 2D battle royale game inspired by surviv.io.
-            You can play it at{" "}
-            <Link href="https://suroi.io" target="_blank" className="text-orange-500">
-              suroi.io
-            </Link>
-            .
-          </p>
-          <p className="text-lg">
-            This wiki is also open-source! Any contributions are <span className="font-bold">appreciated</span>. To contribute, head over to{" "}
-            <Link href="https://github.com/HasangerGames/suroi-wiki" target="_blank" className="text-blue-500">
-              the GitHub repo
-            </Link>
-            .
-          </p>
-        </div>
-        <p className="text-lg text-left mb-4"> {/* Adjusted to left-align text */}
-          Stats are based off Suroi commit <CommitLink sha={HEAD} /> @ <TagLink sha={HEAD} /> 
+        <p>
+          Welcome to the official Suroi wiki! Suroi is an open-source 2D battle
+          royale game inspired by surviv.io. You can play it at{" "}
+          <Link href="https://suroi.io" target="_blank">
+            suroi.io
+          </Link>
+          .
         </p>
-        <h2 className="text-2xl font-bold mt-8 mb-4">Notable Pages</h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-          <PageCard
-            title="Weapons"
-            url="/weapons"
-            image="https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/weapons/ak47.svg"
-            description="List of weapons"
-          />
-          <PageCard
-            title="Healing Items"
-            url="/healing"
-            image="https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/loot/medikit.svg"
-            description="List of healing items"
-          />
-          <PageCard
-            title="Loot Tables"
-            url="/loot"
-            image="https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/obstacles/flint_crate.svg"
-            description="Loot drop rates"
-          />
-          <PageCard
-            title="Armor"
-            url="/equipment/armor"
-            image="https://raw.githubusercontent.com/HasangerGames/suroi/master/client/public/img/game/loot/regular_vest.svg"
-            description="List of armor, including helmets and vests"
-          />
-        </div>
+        <p>
+          This wiki is also open-source! Any contributions are appreciated. To
+          contribute, head over to{" "}
+          <Link
+            href="https://github.com/HasangerGames/suroi-wiki"
+            target="_blank"
+          >
+            the GitHub repo
+          </Link>
+          .
+        </p>
+        <p>
+          Stats are based off Suroi commit <CommitLink sha={HEAD} /> @{" "}
+          <TagLink sha={HEAD} />
+        </p>
+        <h2>Notable Pages</h2>
       </div>
+      <GridTable>
+        {wikiPages.map((page) => (
+          // eslint-disable-next-line react/jsx-key
+          <PageCard
+            title={page.name}
+            url={page.url}
+            image={page.image}
+            description={page.description}
+          />
+        ))}
+      </GridTable>
     </main>
   );
 }
