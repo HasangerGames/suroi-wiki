86ec06f702803ab76e1fce44a1dc0a783217c3b5
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-25 13:04:34 -0700

    I HATE MOBILE

--- components/interactive/NavigationMenu.tsx
+++ components/interactive/NavigationMenu.tsx
@@ -13,7 +13,7 @@ export default function NavigationMenu() {
         <MenuIcon className="w-8 h-8" />
       </button>
       {open && (
-        <div className="fixed top-0 left-0 w-screen h-screen md:w-96 md:h-auto flex flex-col-reverse md:flex-col gap-4 p-4 bg-background rounded-md border-border md:border">
+        <div className="fixed top-0 left-0 w-screen h-screen md:w-96 md:h-auto flex flex-col gap-4 p-4 bg-background rounded-md border-border md:border">
           <button onClick={() => setOpen(false)}>
             <X className="w-8 h-8" />
           </button>
