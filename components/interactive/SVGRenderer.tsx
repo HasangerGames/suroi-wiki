cec51cd967a5d58f37db9362adc61424b5cf0bcd
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-26 19:54:48 -0700

    add more transforms

--- components/interactive/SVGRenderer.tsx
+++ components/interactive/SVGRenderer.tsx
@@ -1,42 +1,58 @@
 import { SVGObject, getImageDimensions } from "@/lib/util/svg";
 
-export default function SVGRenderer({ objects }: SVGRendererProps) {
+export default function SVGRenderer({
+  objects,
+  viewbox,
+  className,
+}: SVGRendererProps) {
+  /*
+  x={getImageDimensions(object.url).then((v) => v.width / -2)}
+  y={getImageDimensions(object.url).then((v) => v.height / -2)}
+  width={getImageDimensions(object.url).then((v) => v.width)}
+  height={getImageDimensions(object.url).then((v) => v.height)}
+  */
   return (
-    <svg>
-      {objects.map((object, i) => (
-        <g
-          key={i.toString()}
-          style={{
-            transformBox: "fill-box",
-            translate: `${object.x}px ${object.y}px`,
-          }}
-        >
-          {(object.type === "circle" && (
-            <circle cx={0} cy={0} radius={object.radius} fill={object.fill} />
-          )) ||
-            (object.type === "rect" && (
-              <rect
-                x={object.width / -2}
-                y={object.height / -2}
-                width={object.width}
-                height={object.height}
-              />
+    <svg viewBox={viewbox} className={className ?? ""}>
+      {[...objects]
+        .sort((a, b) => a.zIndex - b.zIndex)
+        .map((object, i) => (
+          <g
+            key={i.toString()}
+            style={{
+              transformBox: "fill-box",
+              translate: `${object.x}px ${object.y}px`,
+              scale: `${object.scaleX ?? 1} ${object.scaleY ?? 1}`,
+              rotate: `${object.rotation ?? 0}deg`,
+            }}
+          >
+            {(object.type === "circle" && (
+              <circle cx={0} cy={0} radius={object.radius} fill={object.fill} />
             )) ||
-            (object.type === "image" && (
-              <image
-                href={object.url}
-                x={getImageDimensions(object.url).then((v) => v.width / -2)}
-                y={getImageDimensions(object.url).then((v) => v.height / -2)}
-                width={getImageDimensions(object.url).then((v) => v.width)}
-                height={getImageDimensions(object.url).then((v) => v.height)}
-              />
-            ))}
-        </g>
-      ))}
+              (object.type === "rect" && (
+                <rect
+                  x={object.width / -2}
+                  y={object.height / -2}
+                  width={object.width}
+                  height={object.height}
+                />
+              )) ||
+              (object.type === "image" && (
+                <image
+                  x={getImageDimensions(object.url).finally((v) => v.width)}
+                  y={getImageDimensions(object.url).then((v) => v.height / -2)}
+                  width={getImageDimensions(object.url).then((v) => v.width)}
+                  height={getImageDimensions(object.url).then((v) => v.height)}
+                  href={object.url}
+                />
+              ))}
+          </g>
+        ))}
     </svg>
   );
 }
 
 export interface SVGRendererProps extends React.PropsWithChildren {
   objects: SVGObject[];
+  viewbox: string;
+  className?: string;
 }
