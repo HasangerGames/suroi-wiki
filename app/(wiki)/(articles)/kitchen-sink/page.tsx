21302daab404ed81e364878a07b95c65323b3dcf
Compositr <compositr@compositr.dev>
2023-12-29 21:45:03 +1100

    fix: clean up error here

--- app/(wiki)/(articles)/kitchen-sink/page.tsx
+++ app/(wiki)/(articles)/kitchen-sink/page.tsx
@@ -6,95 +6,12 @@ import Empty from "@/components/articles/notices/Empty";
 import Event from "@/components/articles/notices/Event";
 import Removed from "@/components/articles/notices/Removed";
 import Stub from "@/components/articles/notices/Stub";
-import GunGraph from "@/components/interactive/GunGraph";
-import SVGObjectRenderer from "@/components/svg/SVGObjectRenderer";
-import PlayerHoldingGun from "@/components/svg/special/PlayerHoldingGun";
-import PlayerHoldingMelee from "@/components/svg/special/PlayerHoldingMelee";
 import { getSuroiImageLink, getSuroiItem } from "@/lib/util/suroi";
 import { Guns } from "@/vendor/suroi/common/src/definitions/guns";
-import { Melees } from "@/vendor/suroi/common/src/definitions/melees";
-import { Skins } from "@/vendor/suroi/common/src/definitions/skins";
 
 export default async function Kitchen() {
   return (
     <div className="block col-span-full">
-      <PlayerHoldingGun
-        gun={Guns.find((gun) => {
-          return gun.idString === "dual_cz75a";
-        })}
-        skin={Skins.definitions[10]}
-      />
-      <PlayerHoldingMelee
-        melee={Melees.find((melee) => {
-          return melee.idString === "kbar";
-        })}
-        skin={Skins.definitions[10]}
-        use={false}
-      />
-      <PlayerHoldingMelee
-        melee={Melees.find((melee) => {
-          return melee.idString === "kbar";
-        })}
-        skin={Skins.definitions[10]}
-        use={true}
-      />
-      <svg>
-        <SVGObjectRenderer
-          objects={[
-            {
-              type: "image",
-              url: getSuroiImageLink(getSuroiItem("leia"), undefined, "base"),
-              x: 0,
-              y: 0,
-              rotation: 90,
-              zIndex: 3,
-            },
-            {
-              type: "image",
-              url: getSuroiImageLink(getSuroiItem("leia"), undefined, "fist"),
-              x: 35,
-              y: 35,
-              rotation: 90,
-              zIndex: 4,
-            },
-            {
-              type: "image",
-              url: getSuroiImageLink(getSuroiItem("leia"), undefined, "fist"),
-              x: -35,
-              y: 35,
-              rotation: 90,
-              zIndex: 4,
-            },
-          ]}
-        />
-        <SVGObjectRenderer
-          objects={[
-            {
-              type: "image",
-              url: getSuroiImageLink(getSuroiItem("aug"), undefined, "world"),
-              x: 100,
-              y: 100,
-              zIndex: 0,
-            },
-            {
-              type: "image",
-              url: getSuroiImageLink(getSuroiItem("aug")),
-              x: 0,
-              y: 0,
-              scaleX: 2.5,
-              zIndex: 1,
-            },
-            {
-              type: "image",
-              url: getSuroiImageLink(getSuroiItem("medikit")),
-              x: 30,
-              y: 0,
-              zIndex: -1,
-            },
-          ]}
-          viewbox="0 0 800 400"
-        />
-      </svg>
       <DevWeapon />
       <Empty />
       <Event />
