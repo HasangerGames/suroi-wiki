bfa9b27cb1d186344bce62acea23547956f2102e
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-29 12:49:46 -0700

    refactor and add animejs

--- components/svg/special/PlayerHoldingMelee.tsx
+++ components/svg/special/PlayerHoldingMelee.tsx
@@ -1,55 +1,73 @@
+"use client";
+
 import { MeleeDefinition } from "@/vendor/suroi/common/src/definitions/melees";
 import { SkinDefinition } from "@/vendor/suroi/common/src/definitions/skins";
 import SVGObjectRenderer from "../SVGObjectRenderer";
 import { SVGObject } from "@/lib/util/types";
-import { MISSING_TEXTURE, getSuroiImageLink } from "@/lib/util/suroi";
-import { existsSync } from "fs";
+import { getSuroiImageLink } from "@/lib/util/suroi";
+import anime from "animejs";
 
 export default function PlayerHoldingMelee({
   melee,
   skin,
   use,
 }: PlayerHoldingMeleeProps) {
-  const image: SVGObject[] = [
-    {
-      type: "image",
-      url: getSuroiImageLink(skin, undefined, "base"),
-      zIndex: 2,
-    },
-  ];
+  const modes = ["normal", "used", "animated"];
+  const weapon: SVGObject = {
+    type: "image",
+    url: getSuroiImageLink(melee),
+    x:
+      (use ? melee.image?.usePosition.x : melee.image?.position.x) ??
+      0 + melee.offset.x,
+    y:
+      (use ? melee.image?.usePosition.y : melee.image?.position.y) ??
+      0 + melee.offset.y,
+    rotation: use ? melee.image?.useAngle : melee.image?.angle,
+    zIndex: 1,
+  };
 
-  image.push(
-    {
-      type: "image",
-      url: getSuroiImageLink(melee),
-      x:
-        (use ? melee.image?.usePosition.x : melee.image?.position.x) ??
-        0 + melee.offset.x,
-      y:
-        (use ? melee.image?.usePosition.y : melee.image?.position.y) ??
-        0 + melee.offset.y,
-      rotation: use ? melee.image?.useAngle : melee.image?.angle,
-      zIndex: 1,
-    },
-    {
-      type: "image",
-      url: getSuroiImageLink(skin, undefined, "fist"),
-      x: use ? melee.fists.useLeft.x : melee.fists.left.x,
-      y: use ? melee.fists.useLeft.y : melee.fists.left.y,
-      zIndex: 4,
-    },
-    {
-      type: "image",
-      url: getSuroiImageLink(skin, undefined, "fist"),
-      x: use ? melee.fists.useRight.x : melee.fists.right.x,
-      y: use ? melee.fists.useRight.y : melee.fists.right.y,
-      zIndex: 4,
-    }
-  );
+  const leftFist: SVGObject = {
+    type: "image",
+    url: getSuroiImageLink(skin, undefined, "fist"),
+    x: use ? melee.fists.useLeft.x : melee.fists.left.x,
+    y: use ? melee.fists.useLeft.y : melee.fists.left.y,
+    zIndex: 4,
+  };
+
+  const rightFist: SVGObject = {
+    type: "image",
+    url: getSuroiImageLink(skin, undefined, "fist"),
+    x: use ? melee.fists.useRight.x : melee.fists.right.x,
+    y: use ? melee.fists.useRight.y : melee.fists.right.y,
+    zIndex: 4,
+  };
+
+  const weaponAnimation = anime({
+    targets: weapon,
+  });
+
   return (
-    <svg viewBox="-100 -100 300 200">
-      <SVGObjectRenderer objects={image} />
-    </svg>
+    <div>
+      <svg viewBox="-100 -100 300 200">
+        <SVGObjectRenderer
+          objects={[
+            {
+              type: "image",
+              url: getSuroiImageLink(skin, undefined, "base"),
+              zIndex: 2,
+            },
+            leftFist,
+            rightFist,
+            weapon,
+          ]}
+        />
+      </svg>
+      <div className="w-full flex flex-row flex-wrap gap-2">
+        {modes.map((mode) => (
+          <button key={mode}>{mode}</button>
+        ))}
+      </div>
+    </div>
   );
 }
 
