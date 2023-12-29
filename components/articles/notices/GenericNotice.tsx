59ddc6d390f11944fe7423450b15f5f16b3f6f73
Compositr <compositr@compositr.dev>
2023-12-29 21:57:11 +1100

    enhance: add some y margin to article notices

--- components/articles/notices/GenericNotice.tsx
+++ components/articles/notices/GenericNotice.tsx
@@ -2,7 +2,7 @@ import Emote from "@/components/icons/Emote";
 
 export default function GenericNotice({ emote, children }: GenericNoticeProps) {
   return (
-    <div className="flex gap-4 items-center p-2 pl-4 bg-muted">
+    <div className="flex gap-4 items-center p-2 pl-4 my-4 bg-muted">
       <div className="flex items-center">
         <Emote emote={emote} />
       </div>
