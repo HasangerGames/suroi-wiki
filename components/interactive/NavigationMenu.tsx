9cccd9db02fce7998fe6811d7434ae20625e87d3
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-29 14:11:58 -0700

    feat: animated navigation menu

--- components/interactive/NavigationMenu.tsx
+++ components/interactive/NavigationMenu.tsx
@@ -12,25 +12,27 @@ export default function NavigationMenu() {
       <button onClick={() => setOpen(true)} className="hover:text-primary">
         <MenuIcon className="w-8 h-8" />
       </button>
-      {open && (
-        <div className="fixed top-0 left-0 w-screen h-screen md:w-96 md:h-auto flex flex-col gap-4 p-4 bg-background rounded-md border-border md:border">
-          <button onClick={() => setOpen(false)}>
-            <X className="w-8 h-8" />
-          </button>
-          <div className="flex flex-col ml-1">
-            {wikiPages.map((item) => (
-              <Link
-                key={item.url}
-                href={item.url}
-                className="transition-colors text-lg hover:text-primary py-4"
-                onClick={() => setOpen(false)}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
+      <div
+        className={`${
+          open ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-out fixed top-0 left-0 w-screen h-screen md:w-72 flex flex-col gap-4 p-4 bg-background border-border md:border-r`}
+      >
+        <button onClick={() => setOpen(false)}>
+          <X className="w-8 h-8" />
+        </button>
+        <div className="flex flex-col ml-1">
+          {wikiPages.map((item) => (
+            <Link
+              key={item.url}
+              href={item.url}
+              className="transition-colors text-lg hover:text-primary py-4"
+              onClick={() => setOpen(false)}
+            >
+              {item.name}
+            </Link>
+          ))}
         </div>
-      )}
+      </div>
     </>
   );
 }
