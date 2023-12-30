bed1b89b7b8a90aaf1a08525577254619095c39a
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-29 21:37:56 -0700

    increase contrast between infobox and header

--- components/sidebars/utils/GenericSidebar.tsx
+++ components/sidebars/utils/GenericSidebar.tsx
@@ -10,7 +10,7 @@ export default function GenericSidebar({
   return (
     <div className="col-span-2">
       <div className="flex flex-col gap-2 p-2 bg-muted rounded-md">
-        <div className="p-2 bg-white/10 rounded-md">
+        <div className="p-2 bg-white/20 rounded-md">
           <h2 className="text-xl font-bold text-center">{title}</h2>
         </div>
         {(imageVariations && (
