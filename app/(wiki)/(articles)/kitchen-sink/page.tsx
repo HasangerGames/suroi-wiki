594a061d56525887c2db02d1ccdb49868e35c08d
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-30 20:07:00 -0700

    Add matrix table (mobile broken)

--- app/(wiki)/(articles)/kitchen-sink/page.tsx
+++ app/(wiki)/(articles)/kitchen-sink/page.tsx
@@ -7,6 +7,7 @@ import Event from "@/components/articles/notices/Event";
 import Removed from "@/components/articles/notices/Removed";
 import Stub from "@/components/articles/notices/Stub";
 import PlayerWearingEquipment from "@/components/svg/special/PlayerWearingEquipment";
+import MatrixTable from "@/components/tables/MatrixTable";
 import TableWithHeader from "@/components/tables/TableWithHeader";
 import { getSuroiImageLink, getSuroiItem } from "@/lib/util/suroi";
 import { Guns } from "@/vendor/suroi/common/src/definitions/guns";
@@ -19,6 +20,15 @@ export default async function Kitchen() {
         content={[
           ["milly", "cat", "uwu"],
           ["bleh", ":3", "silly"],
+        ]}
+      />
+      <MatrixTable
+        tHeader={["1", "2", "3"]}
+        lHeader={["4", "5", "6"]}
+        content={[
+          ["1", "2", "3"],
+          ["1", "2", "3"],
+          ["1", "2", "3"],
         ]}
       />
       <PlayerWearingEquipment />
