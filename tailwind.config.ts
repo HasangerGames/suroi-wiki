a2a7b6da8979de13b0e054e088607270559d169d
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-30 21:05:31 -0700

    remove header bars (they suck)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,9 +69,9 @@ const config: Config = {
         DEFAULT: {
           css: {
             "max-width": "100%",
-            "h1, h2": {
+            /*"h1, h2": {
               "border-bottom": "1px solid gray",
-            },
+            },*/
           },
         },
       },
