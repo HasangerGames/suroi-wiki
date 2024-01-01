ffdaf6e3e743c6a7278641d4f44dbafd8faee441
Kenos <75338427+kenos1@users.noreply.github.com>
2023-12-31 17:00:59 -0700

    throwables pages

---
+++ app/(wiki)/(articles)/weapons/throwables/[item]/layout.tsx
@@ -0,0 +1,28 @@
+import { Throwables } from "@/vendor/suroi/common/src/definitions/throwables";
+import { notFound } from "next/navigation";
+
+export default function ThrowableLayout({
+  children,
+  params,
+}: {
+  params: {
+    item: string;
+  };
+} & React.PropsWithChildren) {
+  const throwable = Throwables.find((item) => {
+    return item.idString === params.item;
+  });
+  if (!throwable) notFound();
+  return (
+    <>
+      <div className="col-span-4 lg:col-span-6 prose prose-invert">
+        <h1 className="hidden sm:block">{throwable.name}</h1>
+        {children}
+      </div>
+      {/* here because reverse flex-col */}
+      <div className="prose prose-invert sm:hidden">
+        <h1>{throwable.name}</h1>
+      </div>
+    </>
+  );
+}
