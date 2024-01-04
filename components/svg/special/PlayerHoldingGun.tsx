ad46e1f61d392828e55d3b2d2c7e71f04fc7e5af
Compositr <compositr@compositr.dev>
2024-01-04 10:02:47 +0800

    Revert "wip: new SVG renderer"
    This reverts commit aebf6b414ece5263303091c51ad38098bd358874.

--- components/svg/special/PlayerHoldingGun.tsx
+++ components/svg/special/PlayerHoldingGun.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/vendor/suroi/common/src/definitions/skins";
 import { getSuroiImageLink, getSuroiItem } from "@/lib/util/suroi";
 import { SVGObject } from "@/lib/util/types";
-import * as SVG from "../renderer/SVG";
 
 export default function PlayerHoldingGun({ gun, skin }: PlayerHoldingGunProps) {
   const image: SVGObject[] = [
@@ -23,13 +22,10 @@ export default function PlayerHoldingGun({ gun, skin }: PlayerHoldingGunProps) {
     },
   ];
 
-  const singleVariant = gun.isDual
-    ? Guns.find((g) => {
-        return g.idString === gun.singleVariant;
-      })
-    : undefined;
-
   if (gun.isDual) {
+    const singleVariant = Guns.find((g) => {
+      return g.idString === gun.singleVariant;
+    });
     if (!singleVariant?.isDual) {
       // Left
       image.push({
@@ -63,50 +59,7 @@ export default function PlayerHoldingGun({ gun, skin }: PlayerHoldingGunProps) {
       });
     }
   } else {
-  }
-
-  return (
-    <SVG.Renderer viewBox="-45 -45 250 90" className="p-4">
-      <SVG.Image
-        url={getSuroiImageLink(skin, undefined, "base")}
-        x={0}
-        y={0}
-        zIndex={3}
-      />
-
-      {gun.isDual && singleVariant && !singleVariant?.isDual && (
-        <>
-          {/* Left */}
-          <SVG.Image
-            url={getSuroiImageLink(gun, undefined, "world")}
-            x={singleVariant.image.position.x}
-            y={gun.leftRightOffset * -20}
-            zIndex={2}
-          />
-          <SVG.Image
-            url={getSuroiImageLink(skin, undefined, "fist")}
-            x={singleVariant.fists.left.x}
-            y={gun.leftRightOffset * -20}
-            zIndex={singleVariant.fists.leftZIndex ?? 1}
-          />
-
-          {/* Right */}
-          <SVG.Image
-            url={getSuroiImageLink(gun, undefined, "world")}
-            x={singleVariant.image.position.x}
-            y={gun.leftRightOffset * 20}
-            zIndex={2}
-          />
-          <SVG.Image
-            url={getSuroiImageLink(skin, undefined, "fist")}
-            x={singleVariant.fists.right.x}
-            y={gun.leftRightOffset * 20}
-            zIndex={singleVariant.fists.rightZIndex ?? 1}
-          />
-        </>
-      )}
-
-      {/* image.push(
+    image.push(
       {
         type: "image",
         url: getSuroiImageLink(gun, undefined, "world"),
@@ -128,17 +81,12 @@ export default function PlayerHoldingGun({ gun, skin }: PlayerHoldingGunProps) {
         y: gun.fists.left.y,
         zIndex: gun.fists.leftZIndex ?? 1,
       }
-    ); */}
-      {!gun.isDual && (
-        <>
-          <SVG.Image url={getSuroiImageLink(gun, undefined, "world")}
-          x={gun.image.position.x}
-          y={gun.image.position.y}
-          zIndex={2}
-          />
-        </>
-      )}
-    </SVG.Renderer>
+    );
+  }
+  return (
+    <svg viewBox="-45 -45 250 90" className="p-4">
+      <SVGObjectRenderer objects={image} />
+    </svg>
   );
 }
 
