b56da20c45dc26197459945906dd647f0eaf81d8
Kenos <75338427+kenos1@users.noreply.github.com>
2024-01-12 16:30:19 -0700

    feat: move from grid layout to flexbox layout

--- app/(wiki)/layout.tsx
+++ app/(wiki)/layout.tsx
@@ -1,6 +1,6 @@
 export default function ArticleLayout({ children }: React.PropsWithChildren) {
   return (
-    <main className="flex flex-col-reverse gap-4 sm:grid sm:grid-cols-6 lg:grid-cols-8">
+    <main className="flex flex-col-reverse gap-4 md:flex-row">
       {children}
     </main>
   );
