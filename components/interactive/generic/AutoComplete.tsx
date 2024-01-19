675aabcb92967595e1855c5884466cd190ef3753
Kenos <75338427+kenos1@users.noreply.github.com>
2024-01-18 19:57:43 -0700

    guh

--- components/interactive/generic/AutoComplete.tsx
+++ components/interactive/generic/AutoComplete.tsx
@@ -12,7 +12,26 @@ export default function AutoComplete({ items, setter }: AutoCompleteProps) {
   const [input, setInput] = useState("");
   const [selection, setSelection] = useState(0);
   const [isSelected, setIsSelected] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const results: FuseResult<AutoCompleteItem>[] = fuse.search(input);
+
+  const Item = (item: AutoCompleteItem, i: number) => {
+    return (
+      <button
+        onClick={() => {
+          setIsSelected(true)
+          setInput(item.name)
+          setIsOpen(false)
+          setter(item.item)
+        }}
+        className={`w-full text-left p-2 rounded-md hover:bg-white/5 ${
+          selection === i ? "bg-white/10" : ""
+        }`}
+      >
+        {item.specialDisplay ?? item.name}
+      </button>
+    )
+  }
   return (
     <div
       className="relative w-full"
@@ -42,34 +61,31 @@ export default function AutoComplete({ items, setter }: AutoCompleteProps) {
           setInput(e.target.value);
           setIsSelected(false);
           setSelection(0);
+          setIsOpen(true)
         }}
         type="text"
         className="form-input w-full h-full bg-muted rounded-md"
+        onClick={() => {
+          setIsSelected(false);
+          setIsOpen(true)
+        }
+        }
       />
       <ChevronsUpDown className="absolute top-0 right-2 bottom-0 my-auto" />
-      {input.length > 0 && !isSelected && (
-        <div className="absolute z-10 flex flex-col min-w-[20rem] gap-2 -bottom-2 left-0 translate-y-full bg-background border border-border p-2 rounded-md">
-          {results.length > 0 ? (
-            results.map((result, i) => (
-              <button
-                onClick={() => {
-                  setInput(result.item.name);
-                  setIsSelected(true);
-                  setter(result.item.item);
-                }}
-                key={i}
-                className={`w-full text-left p-2 rounded-md hover:bg-white/5 ${
-                  selection === i ? "bg-white/10" : ""
-                }`}
-              >
-                {result.item.specialDisplay ?? result.item.name}
-              </button>
+      {isOpen && 
+        <div className="absolute z-10 flex flex-col max-h-[20rem] overflow-y-scroll w-full gap-2 -bottom-2 left-0 translate-y-full bg-background border border-border p-2 rounded-md">
+          {input.length > 0
+            ? results.length > 0
+              ? results.map((result, i) => (
+                <Item key={i} item={result.item} i={i} />
+              ))
+              : <p>No results found!</p>
+            : items.map((item, i) => (
+                <Item key={i} item={item} i={i} />
             ))
-          ) : (
-            <p>No results found!</p>
-          )}
+          }
         </div>
-      )}
+      }
     </div>
   );
 }
