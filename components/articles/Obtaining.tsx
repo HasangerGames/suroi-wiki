84ecc1a25966ee6410e5495d566c68dbaf4928f4
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-02-06 11:40:06 +0800

    feat: display more digits when the drop rate is too low/high

--- components/articles/Obtaining.tsx
+++ components/articles/Obtaining.tsx
@@ -6,6 +6,18 @@ import TableWithHeader from "../tables/TableWithHeader";
 import { LootLocationsResponse } from "@/app/api/loot/locations/route";
 import { Obstacles } from "@/vendor/suroi/common/src/definitions/obstacles";
 
+function getChanceString(chance: number): string {
+  if (chance == 1) return '100%';
+  if (chance == 0) return '0%';
+
+  let prec = 2;
+  for (let d = 0.1 ** (prec+1); prec < 15; d *= 0.1, prec++) {
+    if (d <= chance && chance < 1 - d) break;
+  }
+
+  return (chance * 100).toFixed(prec) + '%';
+}
+
 export default function Obtaining(props: ObtainingProps) {
   const results = useQuery<LootLocationsResponse>({
     queryKey: ["loottablelocations", props],
@@ -18,7 +30,7 @@ export default function Obtaining(props: ObtainingProps) {
   if (results.isPending) return 'Loading...';
   if (results.error) return `Error: ${results.error.message}`;
   if ('error' in results.data) return `Error: ${ErrorMessages[results.data.error]}`;
-  if (!results.data.length) return `This item cannot be found in any location.`;
+  if (!results.data.length) return 'This item cannot be found in any location.';
 
   return (
     <div className="mb-8">
@@ -26,7 +38,7 @@ export default function Obtaining(props: ObtainingProps) {
         key={props.item}
         header={["Location", "% Chance"]}
         content={results.data.map(({ tableName, chance }) => {
-          const chanceString = (chance * 100).toFixed(2) + '%';
+          const chanceString = getChanceString(chance);
           const name = Obstacles.definitions.find(obs => obs.idString == tableName)?.name;
           return [name ? <Link href={`/obstacles/${tableName}`}>{name}</Link> : tableName, chanceString];
         })}
