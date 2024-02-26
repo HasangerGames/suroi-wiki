5eeb18852a7241797936bf85e99a29470322d96b
Compositr <compositr@compositr.dev>
2024-02-26 16:36:37 +1100

    style: biome but again

--- src/components/generics/markdown.tsx
+++ src/components/generics/markdown.tsx
@@ -8,40 +8,40 @@ import { CONFIG } from "src/config";
 const markdownIt = MarkdownIt(CONFIG.markdownOptions);
 
 export default function Markdown({
-	dir,
-	file,
+  dir,
+  file,
 }: PropsWithChildren<{
-	dir: "wiki" | "changelog";
-	file: string;
+  dir: "wiki" | "changelog";
+  file: string;
 }>) {
-	const mdPath = path.join(process.cwd(), "content", dir, `${file}.md`);
-	return (
-		<div
-			// biome-ignore lint/security/noDangerouslySetInnerHtml: TODO kenos fix your goofy
-dangerouslySetInnerHTML={{
-				__html: markdownIt.render(matter.read(mdPath).content),
-			}}
-		/>
-	);
+  const mdPath = path.join(process.cwd(), "content", dir, `${file}.md`);
+  return (
+    <div
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: TODO kenos fix your goofy
+      dangerouslySetInnerHTML={{
+        __html: markdownIt.render(matter.read(mdPath).content),
+      }}
+    />
+  );
 }
 
 export function WikiMarkdown({
-	title,
-	article,
-	aside,
+  title,
+  article,
+  aside,
 }: PropsWithChildren<{ title: string; article: string; aside?: JSX.Element }>) {
-	const mdPath = path.join(process.cwd(), "content", "wiki", `${article}.md`);
-	return (
-		<article class="flex flex-col-reverse lg:flex-row gap-4 max-w-screen-xl">
-			<div class="prose prose-white max-w-full grow">
-				<h1>{title}</h1>
-				{existsSync(mdPath) ? (
-					<Markdown dir="wiki" file={article} />
-				) : (
-					<p>Empty page (womp womp)</p>
-				)}
-			</div>
-			{aside && <div class="max-w-96">{aside}</div>}
-		</article>
-	);
+  const mdPath = path.join(process.cwd(), "content", "wiki", `${article}.md`);
+  return (
+    <article class="flex flex-col-reverse lg:flex-row gap-4 max-w-screen-xl">
+      <div class="prose prose-white max-w-full grow">
+        <h1>{title}</h1>
+        {existsSync(mdPath) ? (
+          <Markdown dir="wiki" file={article} />
+        ) : (
+          <p>Empty page (womp womp)</p>
+        )}
+      </div>
+      {aside && <div class="max-w-96">{aside}</div>}
+    </article>
+  );
 }
