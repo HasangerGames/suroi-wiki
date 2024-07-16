8e6050659819a0dc168f29ddcff279c50aa982ed
Damien Vesper <ldamienvesper@gmail.com>
2024-07-15 21:59:46 -0500

    refactor: customize eslint, drop prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,18 +3,18 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,md,mdx}"
   ],
   theme: {
     container: {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
-      },
+        "2xl": "1400px"
+      }
     },
     fontFamily: {
-      sans: ["var(--font-inter)", "sans-serif"],
+      sans: ["var(--font-inter)", "sans-serif"]
     },
     extend: {
       colors: {
@@ -25,58 +25,58 @@ const config: Config = {
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          foreground: "hsl(var(--primary-foreground))"
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          foreground: "hsl(var(--secondary-foreground))"
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          foreground: "hsl(var(--destructive-foreground))"
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          foreground: "hsl(var(--muted-foreground))"
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          foreground: "hsl(var(--accent-foreground))"
         },
         popover: {
           DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          foreground: "hsl(var(--popover-foreground))"
         },
         card: {
           DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          foreground: "hsl(var(--card-foreground))"
         },
         suroi: {
-          DEFAULT: "hsl(var(--suroi))",
-        },
+          DEFAULT: "hsl(var(--suroi))"
+        }
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        sm: "calc(var(--radius) - 4px)"
       },
       backgroundImage: {
-        checker: "url(/img/tiny-checkers.svg)", // From https://heropatterns.com/
+        checker: "url(/img/tiny-checkers.svg)" // From https://heropatterns.com/
       },
 
       // Typography
       typography: {
         DEFAULT: {
           css: {
-            "max-width": "100%",
-            /*"h1, h2": {
+            "max-width": "100%"
+            /* "h1, h2": {
               "border-bottom": "1px solid gray",
-            },*/
-          },
-        },
-      },
-    },
+            }, */
+          }
+        }
+      }
+    }
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")]
 };
 export default config;
