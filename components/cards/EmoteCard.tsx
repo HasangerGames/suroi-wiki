18f9ab7f6daa07b4487c13422053ed6ac4d70be8
1092384 <66282302+1092384@users.noreply.github.com>
2024-12-26 18:23:57 +0900

    Revert "deprecate old suroi image provider"
    This reverts commit a332bbacbac86b0d7e266e5460c49b1f13691f31.

--- components/cards/EmoteCard.tsx
+++ components/cards/EmoteCard.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import { getSuroiSprite } from "@/lib/util/suroi";
-import { EmoteDefinition } from "@/vendor/suroi/common/src/definitions/emotes";
+import { getSuroiImageLink } from "@/lib/util/suroi";
 
 export default function EmoteCard({ emote }: EmoteCardProps) {
   return (
     <div className="flex p-4 gap-5 not-prose rounded-md ring ring-border hover:ring-primary">
-      <Image src={getSuroiSprite(emote.idString)} width="100" height="100" alt={emote.name} className="max-w-[10rem]" />
+      <Image src={getSuroiImageLink(emote)} width="100" height="100" alt={emote.name} className="max-w-[10rem]" />
       <div className="flex-1">
         <h3 className="text-lg font-bold break-normal leading-loose underline transition-colors">
           {emote.name}
